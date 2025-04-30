aa583baa7fb1cda560fbd1ea68117c78c74ccf54
alam0164088 <168212623+alam0164088@users.noreply.github.com>
2025-04-30 06:25:23 +0600

    Add gunicorn and configure for Render deploy

--- local_event_finder/settings.py
+++ local_event_finder/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
 # মিডলওয়্যার
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
